c50e03fa829c4e1fab0ec41295322b80aaf59a04
Alex Sanudo <asanudolopez+git@gmail.com>
2025-07-18 15:50:16 -0300

    added production check

--- client/src/api/parts.js
+++ client/src/api/parts.js
@@ -1,7 +1,8 @@
+const BASE_URL = import.meta.env.VITE_API_URL || '';
 export const getPartsFromImage = async image => {
   const formData = new FormData();
   formData.append('vinImage', image);
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/parts/image`, {
+  const res = await fetch(`${BASE_URL}/api/parts/image`, {
     method: 'POST',
     body: formData,
   });
@@ -10,7 +11,7 @@ export const getPartsFromImage = async image => {
 };
 
 export const getPartsFromVin = async vin => {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/parts?vin=${vin}`);
+  const res = await fetch(`${BASE_URL}/api/parts?vin=${vin}`);
   const data = await res.json();
   return data;
 };
