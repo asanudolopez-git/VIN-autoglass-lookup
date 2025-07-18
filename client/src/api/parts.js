2b0f06cc247ab92179be15b7c0559da53cf48b6a
Alex Sanudo <asanudolopez+git@gmail.com>
2025-07-18 16:05:17 -0300

    ok wtf

--- client/src/api/parts.js
+++ client/src/api/parts.js
@@ -1,4 +1,5 @@
 const BASE_URL = import.meta.env.VITE_API_URL || '/api';
+
 export const getPartsFromImage = async image => {
   const formData = new FormData();
   formData.append('vinImage', image);
@@ -11,6 +12,11 @@ export const getPartsFromImage = async image => {
 };
 
 export const getPartsFromVin = async vin => {
+  const str = `${BASE_URL}/parts?vin=${vin}`;
+  console.log('BASE_URL: ', BASE_URL);
+  console.log('str: ', `${BASE_URL}/parts?vin=${vin}`)
+  console.log('import.meta.env.VITE_API_URL', import.meta.env.VITE_API_URL);
+  console.log(`${BASE_URL}/parts?vin=${vin}`)
   const res = await fetch(`${BASE_URL}/parts?vin=${vin}`);
   const data = await res.json();
   return data;
