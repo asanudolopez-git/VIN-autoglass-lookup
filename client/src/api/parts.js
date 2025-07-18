4ca3a5b21cf70729613c6759122a491ca57923dd
Alex Sanudo <asanudolopez+git@gmail.com>
2025-07-18 15:59:04 -0300

    fixed base url

--- client/src/api/parts.js
+++ client/src/api/parts.js
@@ -1,8 +1,8 @@
-const BASE_URL = import.meta.env.VITE_API_URL || '';
+const BASE_URL = import.meta.env.VITE_API_URL || '/api';
 export const getPartsFromImage = async image => {
   const formData = new FormData();
   formData.append('vinImage', image);
-  const res = await fetch(`${BASE_URL}api/parts/image`, {
+  const res = await fetch(`${BASE_URL}/parts/image`, {
     method: 'POST',
     body: formData,
   });
@@ -11,7 +11,7 @@ export const getPartsFromImage = async image => {
 };
 
 export const getPartsFromVin = async vin => {
-  const res = await fetch(`${BASE_URL}api/parts?vin=${vin}`);
+  const res = await fetch(`${BASE_URL}/parts?vin=${vin}`);
   const data = await res.json();
   return data;
 };
